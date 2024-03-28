b5a129cdd123ca38b14ca6b40a98929018709c71
DzulFaqor01 <bimoarijanto236@gmail.com>
2024-03-28 16:16:59 +0700

    kurang data anggota

--- application/views/templates/templates-user/header.php
+++ application/views/templates/templates-user/header.php
@@ -21,12 +21,12 @@
             </button>
             <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
                 <div class="navbar-nav">
-                    <a class="nav-item navlink active" href="<?= base_url(); ?>">Beranda <span class="sronly">(current)</span></a>
+                    <a class="nav-item nav-link active" href="<?= base_url(); ?>">Beranda <span class="sr-only">(current)</span></a>
                     <?php
                     if (!empty($this->session->userdata('email'))) { ?>
                         <a class="nav-item nav-link" href="#">Booking Buku</a>
-                        <a class="nav-item navlink" href="<?= base_url('member/myprofil'); ?>">Profil Saya</a>
-                        <a class="nav-item navlink" href="<?= base_url('member/logout'); ?>"><i class="fas fw falogin"></i> Log out</a><?php } else { ?>
+                        <a class="nav-item nav-link" href="<?= base_url('member/myprofil'); ?>">Profil Saya</a>
+                        <a class="nav-item nav-link" href="<?= base_url('member/logout'); ?>"><i class="fas fw falogin"></i> Log out</a><?php } else { ?>
                         <a class="nav-item nav-link" data-toggle="modal" datatarget="#daftarModal" href="#"><i class="fas fw fa-login"></i> Daftar</a>
                         <a class="nav-item nav-link" data-toggle="modal" datatarget="#loginModal" href="#"><i class="fas fw fa-login"></i> Log in</a>
                     <?php } ?>
